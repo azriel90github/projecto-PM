2e15337c3bef4b7111127ebf8759b41797919e78
Azriel Armando <114200974+azriel90github@users.noreply.github.com>
2025-04-10 20:17:57 +0100

    final update

--- js/scroller.js
+++ js/scroller.js
@@ -1,4 +1,4 @@
-const scrollers = document.querySelectorAll(".scroller");
+ const scrollers = document.querySelectorAll(".scroller");
 
 // If a user hasn't opted in for recuded motion, then we add the animation
 if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
